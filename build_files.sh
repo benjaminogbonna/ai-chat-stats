c87792fffaec47ed0b310c7d0fdbc039e5c0ede0
Benjamin <onyedikachi1997@gmail.com>
2025-01-25 04:27:09 +0100

    vercel configuration.

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo " BUILD START"
-python3.9 -m venv venv
-source venv/bin/activate
+# python3.9 -m venv venv
+# source venv/bin/activate
 pip install -r requirements_dev.txt
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
