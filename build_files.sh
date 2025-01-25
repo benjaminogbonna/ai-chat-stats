4cd5ebd55ceccd186eef4c5f5fff2ff0ac057bd4
Benjamin <onyedikachi1997@gmail.com>
2025-01-25 05:33:56 +0100

    vercel staticfiles configuration.

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,8 @@
 echo " BUILD START"
 python3.9 -m venv venv
 source venv/bin/activate
-pip install -r requirements_dev.txt
+# pip install -r requirements_dev.txt
+pip list
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 python3.9 manage.py collectstatic --noinput
